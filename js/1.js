1b1c422d828b392a0b4e725766b36783cc49c1e4
«Julia» <jubarmina@yandex.ru>
2023-11-14 23:51:23 +0300

    ввели решение 2 задачи

--- js/1.js
+++ js/1.js
@@ -4,3 +4,8 @@ function task1() {
     let dayOfWeek=["воскресенье","понедельник", "вторник", "среда","четверг", "пятница", "суббота"];
     alert(getDayOfWeek(data));
 }
+function task2() {
+    let dataNow=new Date();
+    let dataNewYear=new Date(2024,0,1);
+    alert((dataNewYear-dataNow)/(1000*60*60*24));
+}
